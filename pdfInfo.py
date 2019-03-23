307d28cc7488fbdfda9a39c5742dbb6841efb18d
kod2nd <kod2nd@gmail.com>
2019-03-23 11:03:50 +0800

    get word locations

--- pdfInfo.py
+++ pdfInfo.py
@@ -61,32 +61,46 @@ class PdfInfo():
                 temp.append(char)
                 charTop = char["top"]
         self.convertToJson(lines, "lines.json")
-        self.lines = lines
-        return lines
-    
-    def char2Words(self):
-        firstLine = self.lines["0"]
+        formattedLines = [self.char2Words(lineNum, lines) for lineNum in lines]
+        self.formattedLines = formattedLines
+        return formattedLines
+
+    def char2Words(self, lineNum, lines):
+        #        print("Line:", type(lineNum))
+        line = lines[lineNum]
         indexOfSpaces = []
-        for index,char in enumerate(firstLine):
+        for index, char in enumerate(line):
             if char["text"] == " ":
                 indexOfSpaces.append(index)
-        print(indexOfSpaces)
         currentIndex = 0
         words = []
         for spaceIndex in indexOfSpaces:
-            if spaceIndex < len(firstLine):
-                print("HERE", currentIndex, spaceIndex)
-                words.append(firstLine[currentIndex:spaceIndex])
+            if spaceIndex < len(line):
+                words.append(line[currentIndex:spaceIndex])
                 currentIndex = spaceIndex + 1
             else:
-                if firstLine[spaceIndex+1::]:
-                    words.append(firstLine[spaceIndex+1::])
-        # print(len(firstLine),firstLine[56:62])
-        print(words)
+                if line[spaceIndex+1::]:
+                    words.append(line[spaceIndex+1::])
+        joinedWords = [self.joinWord(word) for word in words]
+        return joinedWords
+
+    def joinWord(self, word):
+        firstChar = word[0]
+        lastChar = word[-1]
+        joinedWord = {
+            "x0": firstChar["x0"],
+            "y0": firstChar["y0"],
+            "x1": lastChar["x1"],
+            "y1": lastChar["y1"],
+            "text": ""
+        }
+        for char in word:
+            joinedWord["text"] += char["text"]
+        return joinedWord
 
 
 instance1 = PdfInfo()
 
 instance1.controller("article.pdf")
-instance1.collateLines()
-instance1.char2Words()
+a = instance1.collateLines()
+# instance1.char2Words()
